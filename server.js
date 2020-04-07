365e7c34f90357519b37581d06939be53dfaf741
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-04-07 17:01:42 -0400

    fixed my gets

--- server.js
+++ server.js
@@ -1,48 +1,43 @@
 const express = require("express");
+const path = require("path");
 const fs = require("fs");
 
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 8000;
 
-// let server =http.createServer(handleRequest);
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
 
-//placing the requests
 
-// function
 
-//fs read index html
-function displayIndex(res){
-fs.readFile(__dirname+"/public/index.html",function(err, data) {
-    if (err) throw err;
-    // We then respond to the client with the HTML page by specifically telling the browser that we are delivering
-    // an html file.
-    // res.writeFile(200, { "Content-Type": "text/html" });
-    res.end(data);
-  });
-}
-function displayNotes(res){
-    fs.readFile(__dirname+"/public/notes.html",function(err, data) {
+
+
+
+//will access the page where the notes are located(html route)
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+//will read the db and put the object within in your notes page.(api route)
+app.get("/api/notes", function (req, res) {
+    fs.readFile("db/db.json", function (err, data) {
         if (err) throw err;
-        // We then respond to the client with the HTML page by specifically telling the browser that we are delivering
-        // an html file.
-        // res.writeFile(200, { "Content-Type": "text/html" });
-        res.end(data);
-      });
-    }
-
-//routes
-
-app.get("*", function(req,res){
-    res.send(displayIndex(res));
+      res.json(JSON.parse(data));
+    });
 })
 
+//(api route)  Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
 
-app.get("/notes", function(req, res) {
-    res.send(displayNotes(res));
-})
+
+//will return to main page(html rout)
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
 
 //listener:
-app.listen(PORT, function(){
-    console.log("it's working on PORT: "+PORT);
+app.listen(PORT, function () {
+    console.log("it's working on PORT: " + PORT);
 });
+
