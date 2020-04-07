dd6f9cf21ac94d3d242e5fcea01c9101338602c8
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-04-06 21:44:09 -0400

    listener is working

--- server.js
+++ server.js
@@ -5,36 +5,41 @@ const fs = require("fs");
 const app = express();
 const PORT = 3000;
 
-let server =http.createServer(handleRequest);
+// let server =http.createServer(handleRequest);
 
 //placing the requests
 
-function
+// function
 
 //fs read index html
 function displayIndex(res){
-fs.readFile(__dirname+"/index.html",function(err, data) {
+fs.readFile(__dirname+"/public/index.html",function(err, data) {
     if (err) throw err;
     // We then respond to the client with the HTML page by specifically telling the browser that we are delivering
     // an html file.
-    res.writeHead(200, { "Content-Type": "text/html" });
+    // res.writeFile(200, { "Content-Type": "text/html" });
     res.end(data);
   });
 }
 function displayNotes(res){
-    fs.readFile(__dirname+"/notes.html",function(err, data) {
+    fs.readFile(__dirname+"/public/notes.html",function(err, data) {
         if (err) throw err;
         // We then respond to the client with the HTML page by specifically telling the browser that we are delivering
         // an html file.
-        res.writeHead(200, { "Content-Type": "text/html" });
+        // res.writeFile(200, { "Content-Type": "text/html" });
         res.end(data);
       });
     }
 
 //routes
 
+app.get("*", function(req,res){
+    res.send(displayIndex(res));
+})
+
+
 app.get("/notes", function(req, res) {
-    res.send()
+    res.send(displayNotes(res));
 })
 
 //listener:
