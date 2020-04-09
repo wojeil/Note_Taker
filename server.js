a6830b9205329260da1d8a70364650172226e297
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-04-08 20:38:11 -0400

    code has been cleaned up

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-
 //will access the page where the notes are located(html route)
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"));
@@ -24,13 +23,11 @@ app.get("/api/notes", function (req, res) {
     });
 })
 
-
 //(api route)  Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
 
 app.post("/api/notes", function (req, res) {
     //the notes that are being inputed
     var newNotes = req.body;
-
 
     fs.readFile("db/db.json", function (err, data) {
         if (err) throw err;
@@ -40,19 +37,13 @@ app.post("/api/notes", function (req, res) {
         notes.push(newNotes);
         console.log(notes);
 
-
         fs.writeFile("db/db.json", JSON.stringify(notes, null, "\t"), function (err) {
             if (err) throw err;
-
             return res.json(notes);
         })
-
     })
-
 });
-
 //API route for Delete through id
-
 app.delete("/api/notes/:id", function (req, res) {
     //the chosen id:
     var chosenOne = req.params.id;
@@ -60,36 +51,28 @@ app.delete("/api/notes/:id", function (req, res) {
         if (err) throw err;
         //the Array of objects in DB
         var notes = JSON.parse(data);
-        //f
+        //function that will create a new array with the the selected option (index from previous array of objects)to be deleted
         var newArray = function () {
             for (var i = 0; i < notes.length; i++) {
                 if (chosenOne == notes[i].id) {
-                    //  delete notes[i];
-                    var newTest = notes.splice(i, 1);
-                    return newTest;
-
+                    var addedArray = notes.splice(i, 1);
+                    return addedArray;
                 }
-                // console.log(newTest);
+                // console.log(addedArray);
                 // console.log(chosenOne);
-                // console.log(notes);
-                // console.log(notes[i].id);
             }
         }
-       console.log(newArray());
+        //call back function for loop to activate and splice to work
+        newArray();
+
         // console.log(notes);
         fs.writeFile("db/db.json", JSON.stringify(notes, null, "\t"), function (err) {
             if (err) throw err;
 
             return res.json(notes);
         })
-
-
     })
-
 })
-
-
-
 //will return to main page(html rout)
 app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
